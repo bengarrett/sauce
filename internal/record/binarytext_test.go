c3b3e93cbdd6bfff1facdd92235dd9db21aea14a
Ben Garrett <code.by.ben@gmail.com>
2021-12-03 07:42:49 +1100

    Binary text is idomatic. Created test coverage.

---
+++ internal/record/binarytext_test.go
@@ -0,0 +1,24 @@
+// This is a raw memory copy of a text mode screen. Also known as a .BIN file.
+// This is essentially a collection of character and attribute pairs.
+// See http://www.acid.org/info/sauce/sauce.htm#FileType
+package record
+
+import "testing"
+
+func TestBinaryText_String(t *testing.T) {
+	tests := []struct {
+		name string
+		b    BinaryText
+		want string
+	}{
+		{"out of range", 999, ""},
+		{"first and last", 0, "Binary text or a .BIN file"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.b.String(); got != tt.want {
+				t.Errorf("BinaryText.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
